ebdba9af2d609bb61ced1f29c90cb8ee2bf208c5
Mohamed Salama <mohamed.salama848@gmail.com>
2022-05-28 11:03:51 +0000

    Update tomcat-server.sh

--- tomcat-server.sh
+++ tomcat-server.sh
@@ -1,80 +1,109 @@
-#!/bin/bash
+# create-tomcat-linux
 
-##
-## should pass 2 parameters with this script
-## 1st one is the path for apache tomcat zip file
-## 2nd one is the user that will created and give tomcat permissions based on its name
-##
+Create tomcat instance on opt (You can change it from source code before you run it) folder with fix permissions on tomcat folders based on created user that created on linux servers (tested with ubuntu, parrot, redhat 8, centos7, centos8)
 
-echo "== start work on tomcat on location $1 with user $2 =="
+## Usage
 
-#comment this step if your user already created in your linux system
-echo "== create group and user with the user $2 =="
-# create group and user with the param2
-groupadd $2
-mkdir /opt/$2-tomcat
-useradd -s /bin/nologin -g $2 -d /home/$2 $2
+First you can download tomcat from this [link](https://downloads.apache.org/tomcat/)
+choose your tomcat version and download it. You will find tomcat ```tar.gz``` file located under ```bin``` folder after you choose your tomcat version. To download it you can use ```wget``` command as example below
 
-sleep 2
+```
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.63/bin/apache-tomcat-9.0.63.tar.gz
+```
 
-echo "== extract tomcat =="
-#extract tomcat with location in param1
-tar -zxvf $1 -C /opt/$2-tomcat --strip-components=1 > /dev/null
-echo "== extract done =="
+To run this script you should run it as below
 
-sleep 1
+```bash
+sh tomcat-server.sh {path-for-tomcat-zip-file} {user-name} 
+```
 
-echo "== fix tomcat permissions for user $2 =="
-#fix permissions for tomcat with parma1
-cd /opt/$2-tomcat
+Example on how to run
 
-chgrp -R $2 conf
-chmod g+rwx conf
-chmod g+r conf/*
-chown -R $2 logs/ temp/ webapps/ work/
+```bash
+sh tomcat-server.sh apache-tomcat-9.0.63.tar.gz user1
+```
 
-chgrp -R $2 bin
-chgrp -R $2 lib
-chmod g+rwx bin
-chmod g+r bin/*
+```apache-tomcat-9.0.63.tar.gz``` here is just example
 
-sleep 2
+## Example output
 
-echo "== create tomcat service =="
-touch /etc/systemd/system/$2-tomcat.service
+```python
+== start work on tomcat on location apache-tomcat-9.x.xx.tar.gz with user user1 ==
+== create group and user with the user user1 ==
+== extract tomcat ==
+== extract done ==
+== fix tomcat permissions for user user1 ==
+== create tomcat service ==
+== try start tomcat service user1-tomcat.service located on /etc/systemd/system/user1-tomcat.service ==
+check tomcat service status
 
-echo "
-[Unit]
-Description=$2 Apache Tomcat Web Application Container
-After=syslog.target network.target
+● user1-tomcat.service - user1 Apache Tomcat Web Application Container
+   Loaded: loaded (/etc/systemd/system/user1-tomcat.service; disabled; vendor preset: disabled)
+   Active: active (running) since Sat 2022-05-14 18:52:25 GMT; 2s ago
+  Process: 3395134 ExecStart=/opt/user1-tomcat/bin/startup.sh (code=exited, status=0/SUCCESS)
+ Main PID: 3395141 (java)
+    Tasks: 32 (limit: 37060)
+   Memory: 136.5M
+   CGroup: /system.slice/user1-tomcat.service
+           └─3395141 /usr/lib/jvm/jre/bin/java -Djava.util.logging.config.file=/opt/user1-tomcat/c>
 
-[Service]
-Type=forking
+May 14 18:52:25 instance-20220423-0021 systemd[1]: Starting user1 Apache Tomcat Web Application Co>
+May 14 18:52:25 instance-20220423-0021 systemd[1]: Started user1 Apache Tomcat Web Application Con>
+== finished create apache-tomcat-9.x.xx.tar.gz tomcat on /opt/user1-tomcat with user user1 ==
+```
 
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-Environment=CATALINA_PID=/opt/$2-tomcat/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/$2-tomcat
-Environment=CATALINA_BASE=/opt/$2-tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -XX:MaxDirectMemorySize=512M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+# Informations to know
 
-ExecStart=/opt/$2-tomcat/bin/startup.sh
-ExecStop=/bin/kill -15 $MAINPID
+Tomcat service path ```/etc/systemd/system/user1-tomcat.service```
 
-User=$2
-Group=$2
+Tomcat path	```/opt/user1-tomcat```
 
-[Install]
-WantedBy=multi-user.target
-" >> /etc/systemd/system/$2-tomcat.service
+After script run it will try to start tomcat service so if ```8080``` is reserved on your server tomcat won't start in this case and start service will fail because of the port reservation. You will need to change port number in tomcat and try to start service again.
 
-sleep 2
+Tomcat server port located on ```/config/server.xml``` file under tomcat folders.
 
-echo "== try start tomcat service $2-tomcat.service located on /etc/systemd/system/$2-tomcat.service =="
-systemctl start $2-tomcat.service
+To start service you will need to run below command by root user or you can ```sudo``` it.
+```
+systemctl start user1-tomcat.service
+```
 
-sleep 2
-echo "check tomcat service status"
-systemctl status $2-tomcat.service
+## Tips tricks
 
-echo "== finished create $1 tomcat on /opt/$2-tomcat with user $2 =="
+Service take any jvm args like below
+```
+-Xms512M -Xmx1024M -XX:MaxDirectMemorySize=512M -server -XX:+UseParallelGC
+```
+Lets describe them may be you need to change them or add another on your environment.
+
+For -Xms512M : specifies the initial memory allocation pool.
+
+-Xmx1024M : specifies the maximum memory allocation pool for a Java Virtual Machine (JVM).
+
+-XX:MaxDirectMemorySize=512M : the limit on memory that can be reserved for all Direct Byte Buffers.
+
+-XX:+UseParallelGC : will turn on the parallel garbage collection.
+
+
+## Environments and versions
+Linux environments that this scripts tested with is 
+
+- Ubuntu, Ubuntu server
+- Centos 7 & 8
+- Parrot
+- Redhat 8
+
+and tomcat versions is
+- tomcat 7
+- tomcat 8
+- tomcat 9
+- tomcat 10
+
+
+## Contributing
+
+Contributions are always welcome!
+
+Please make sure to update ```README``` as appropriate.
+
+## License
+[Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)
